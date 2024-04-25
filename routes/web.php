a420ac4b66a85ffa252cc7dafbe3ef66ce544325
ShroukMohamedAbdelhamied <shroukmousa2017@gmail.com>
2024-04-25 18:10:58 +0200

    massse

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('Shrouk/{id}', function ($id) {
+    return 'welcome to my first web site ' . $id;
+});
